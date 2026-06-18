59ffa2ad20374d027a04ef14469e4b8c1897fdcc
DEEPESH-GOEL <deepeshgoel.13@gmail.com>
2026-06-18 21:07:12 +0530

    initial workspace config using Harness module

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+
+provider "aws" { region = var.region }
+
+module "dg-ec2-instance-harness-repo" {
+  source  = "module.app.harness.io/l7HREAyVTnyfUsfUtPZUowN0dbHpRgRDOgg09wZK__gg/dg-ec2-instance-harness-repo/aws"
+  version = "v1.0.0"
+}
+
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region"        { default = "us-east-1" }
+variable "ami"           { default = "ami-0c02fb55956c7d316" }
+variable "instance_type" { default = "t3.micro" }
+variable "instance_name" { default = "my-ec2" }
+variable "environment"   { default = "dev" }
