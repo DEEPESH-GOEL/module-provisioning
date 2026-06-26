b6b37a53c2bd4471cef7bd5eb15780646435d1a3
DEEPESH-GOEL <deepeshgoel.13@gmail.com>
2026-06-26 13:59:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ terraform {
 provider "aws" { region = var.region }
 
 module "dg-ec2-instance-harness-repo" {
+  instance_name = "Ec2-from-module"
   source  = "module.harness.io/l7HREAyVTnyfUsfUtPZUowN0dbHpRgRDOgg09wZK__gg/dg-ec2-instance-harness-repo/aws"
   version = "v1.0.0"
 }
